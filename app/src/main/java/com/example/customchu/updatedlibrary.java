8a826697cd6d2df642f15ca0e4bcbca88397647f
kreios77 <don.daniel.eullo@adamson.edu.ph>
2023-11-26 13:55:13 +0800

    map overhaul

---
+++ app/src/main/java/com/example/customchu/updatedlibrary.java
@@ -0,0 +1,120 @@
+package com.example.customchu;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jsibbold.zoomage.ZoomageView;
+
+public class updatedlibrary extends AppCompatActivity {
+
+    ImageView crowdLogo;
+    DatabaseReference databaseFacility;
+    int libRoom1;
+    TextView floor1count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatedlibrary);
+
+        //Navigation
+        ImageButton mapBack = findViewById(R.id.toHome1);
+        mapBack.setOnClickListener(view -> {
+            Intent intent = new Intent(updatedlibrary.this, home.class);
+            startActivity(intent);
+        });
+
+        Button nextbutton = findViewById(R.id.to2ndFloor);
+        nextbutton.setOnClickListener(view -> {
+            Intent intent = new Intent(updatedlibrary.this, updatedlibraryb.class);
+            startActivity(intent);
+        });
+
+        ZoomageView MapViewer = findViewById(R.id.img_view);
+
+        // Declare the radiobs
+        RadioButton radioNone = findViewById(R.id.radiobuttonnone);
+        RadioButton radioSeats = findViewById(R.id.radiobuttonseats);
+        RadioButton radioScanner = findViewById(R.id.radiobuttonscanner);
+        RadioButton radioOpac = findViewById(R.id.radiobuttonopac);
+
+        radioSeats.setChecked(true);
+        // Events for clicks
+        radioNone.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.svgf1base));
+        radioSeats.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.svgf1seats));
+        radioScanner.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.svgf1scanner));
+        radioOpac.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.svgf1opac));
+
+        radioSeats.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Remove the listener to avoid multiple calls
+                radioSeats.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                // Perform click after the layout is complete
+                radioSeats.performClick();
+            }
+        });
+
+
+        crowdLogo = findViewById(R.id.crowdLogo);
+        floor1count = findViewById(R.id.floor1count);
+
+        databaseFacility = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference room1 = databaseFacility.child("Rooms").child("GF").child("Current");
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                floor1count.setText("Current Occupied: " + dataSnapshot.getValue() + "");
+                libRoom1 = Integer.parseInt(dataSnapshot.getValue() + "");
+                if (libRoom1 < 21) {
+                    //crowdLogo.setColorFilter(Color.argb(1, 56, 142, 60));
+                    floor1count.setTextColor(getResources().getColor(R.color.green));
+                    ImageViewCompat.setImageTintMode(crowdLogo, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo, ColorStateList.valueOf(Color.parseColor("#388E3C")));
+                } else if (libRoom1 < 36) {
+                   // crowdLogo.setColorFilter(Color.argb(1, 218, 194, 31));
+                    floor1count.setTextColor(getResources().getColor(R.color.yellow));
+                    ImageViewCompat.setImageTintMode(crowdLogo, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo, ColorStateList.valueOf(Color.parseColor("#DAC21F")));
+
+                } else {
+                    //crowdLogo.setColorFilter(Color.argb(1, 211, 47, 47));
+                    floor1count.setTextColor(getResources().getColor(R.color.red));
+                    ImageViewCompat.setImageTintMode(crowdLogo, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo, ColorStateList.valueOf(Color.parseColor("#D32F2F")));
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        room1.addValueEventListener(postListener);
+    }
+}
---
+++ app/src/main/java/com/example/customchu/updatedlibraryb.java
@@ -0,0 +1,121 @@
+package com.example.customchu;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jsibbold.zoomage.ZoomageView;
+
+public class updatedlibraryb extends AppCompatActivity {
+    ImageView crowdLogo2;
+    DatabaseReference databaseFacility;
+    int libRoom2;
+    TextView floor2count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatedlibraryb);
+
+        //Navigation
+        ImageButton mapBack = findViewById(R.id.toHome1);
+        mapBack.setOnClickListener(view -> {
+            Intent intent = new Intent(updatedlibraryb.this, home.class);
+            startActivity(intent);
+        });
+
+        Button nextbutton = findViewById(R.id.to2ndFloor);
+        nextbutton.setOnClickListener(view -> {
+            Intent intent = new Intent(updatedlibraryb.this, updatedlibrary.class);
+            startActivity(intent);
+        });
+
+        ZoomageView MapViewer = findViewById(R.id.img_view);
+
+        // Declare the radiobs
+        RadioButton radioNone = findViewById(R.id.radiobuttonnone);
+        RadioButton radioSeats = findViewById(R.id.radiobuttonseats);
+        RadioButton radioScanner = findViewById(R.id.radiobuttonscanner);
+        RadioButton radioOpac = findViewById(R.id.radiobuttonopac);
+
+        radioSeats.setChecked(true);
+
+        // Events for clicks
+        radioNone.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.f2n3base));
+        radioSeats.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.f2n3seats));
+        radioScanner.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.f2n3scanner));
+        radioOpac.setOnClickListener(view -> MapViewer.setImageResource(R.drawable.f2n3opac));
+
+        radioSeats.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Remove the listener to avoid multiple calls
+                radioSeats.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                // Perform click after the layout is complete
+                radioSeats.performClick();
+            }
+        });
+
+        crowdLogo2 = findViewById(R.id.crowdLogo2);
+        floor2count = findViewById(R.id.floor2count);
+
+        databaseFacility = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference room2 = databaseFacility.child("Rooms").child("2F").child("Current");
+
+        ValueEventListener postListener2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                floor2count.setText("Current Occupied: " + dataSnapshot.getValue() + "");
+                libRoom2 = Integer.parseInt(dataSnapshot.getValue() + "");
+                if (libRoom2 < 26)
+                {
+                    floor2count.setTextColor(getResources().getColor(R.color.green));
+                    ImageViewCompat.setImageTintMode(crowdLogo2, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo2, ColorStateList.valueOf(Color.parseColor("#388E3C")));
+
+                }
+                else if (libRoom2 < 51)
+                {
+                    floor2count.setTextColor(getResources().getColor(R.color.yellow));
+                    ImageViewCompat.setImageTintMode(crowdLogo2, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo2, ColorStateList.valueOf(Color.parseColor("#DAC21F")));
+
+                }
+                else
+                {
+                    floor2count.setTextColor(getResources().getColor(R.color.red));
+                    ImageViewCompat.setImageTintMode(crowdLogo2, PorterDuff.Mode.SRC_ATOP);
+                    ImageViewCompat.setImageTintList(crowdLogo2, ColorStateList.valueOf(Color.parseColor("#D32F2F")));
+
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+
+        };
+        room2.addValueEventListener(postListener2);
+    }
+}
