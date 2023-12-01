e66b6187f17c4f70d89238763b721d97195e4dcc
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-12-02 06:25:19 +0800

    feat: update rooms upon checkin & out

--- app/src/main/java/com/example/customchu/QRActivity.java
+++ app/src/main/java/com/example/customchu/QRActivity.java
@@ -2,30 +2,34 @@ package com.example.customchu;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.zxing.Result;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class QRActivity extends AppCompatActivity {
@@ -33,33 +37,35 @@ public class QRActivity extends AppCompatActivity {
     TextView txtScan;
     DatabaseReference databaseFacility;
     int libRoom1 = 0, libRoom2 = 0;
+    int capLibRoom1 = 50, capLibRoom2 = 50;
     private CodeScanner mCodeScanner;
+    GoogleSignInAccount user;
+
+    DatabaseReference room1, room2, capRoom1, capRoom2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qr);
 
+        // fetch the logged in user
+        user = GoogleSignIn.getLastSignedInAccount(this);
+
         txtScan = findViewById(R.id.txtScan);
 
         setupPermissions();
 
         qrBack = findViewById(R.id.qrBack);
-        qrBack.setOnClickListener(view -> {
-//            Intent intent = new Intent(QRActivity.this, home.class);
-//            startActivity(intent);
-            finish();
-        });
+        qrBack.setOnClickListener(view -> finish());
 
         databaseFacility = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference room1 = databaseFacility.child("Rooms").child("GF").child("Current");
-        DatabaseReference room2 = databaseFacility.child("Rooms").child("2F").child("Current");
 
         ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                libRoom1 = Integer.parseInt(dataSnapshot.getValue() + "");
+                libRoom1 = Integer.parseInt(dataSnapshot.child("Current").getValue().toString());
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
@@ -67,13 +73,31 @@ public class QRActivity extends AppCompatActivity {
             }
 
         };
+        room1 = databaseFacility.child("Rooms").child("GF");
         room1.addValueEventListener(postListener);
+
+        ValueEventListener postListener3 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                capLibRoom1 = Integer.parseInt(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+
+        };
+        capRoom1 = databaseFacility.child("Rooms").child("GF").child("Cap");
+        capRoom1.addValueEventListener(postListener3);
 
         ValueEventListener postListener2 = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                libRoom2 = Integer.parseInt(dataSnapshot.getValue() + "");
+                libRoom2 = Integer.parseInt(dataSnapshot.child("Current").getValue().toString());
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
@@ -81,7 +105,25 @@ public class QRActivity extends AppCompatActivity {
             }
 
         };
+        room2 = databaseFacility.child("Rooms").child("2F");
         room2.addValueEventListener(postListener2);
+
+        ValueEventListener postListener4 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                capLibRoom2 = Integer.parseInt(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+
+        };
+        capRoom2 = databaseFacility.child("Rooms").child("2F").child("Cap");
+        capRoom2.addValueEventListener(postListener4);
+
 
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
@@ -89,43 +131,76 @@ public class QRActivity extends AppCompatActivity {
             String scannedContent = result.getText();
             String expectedContent = "Library Room1";
             if (scannedContent.equalsIgnoreCase("Library Ground Floor")) {
+                // check if room is full
+                if (libRoom1 >= capLibRoom1) {
+                    // show message that the room is full
+                    txtScan.setText("Room is full, try again later");
+                    return;
+                }
+
                 txtScan.setText("QR Code successfully scanned: " + scannedContent);
-                room1.setValue(libRoom1 + 1);
-                //successful notif muna dapat dito
+
+                // insert to database
+                room1.child("Current").setValue(libRoom1 + 1);
+                insertOnRoom1();
+
                 Intent intent = new Intent(QRActivity.this, mapActivity.class);
                 startActivity(intent);
             } else if (scannedContent.equalsIgnoreCase("Library Second Floor")) {
+                // check if room is full
+                if (libRoom2 >= capLibRoom2) {
+                    txtScan.setText("Room is full, try again later");
+                    return;
+                }
+
                 txtScan.setText("QR Code successfully scanned: " + scannedContent);
-                room2.setValue(libRoom2 + 1);
-                //successful notif muna dapat dito
+
+                // insert to database
+                room2.child("Current").setValue(libRoom2 + 1);
+                insertOnRoom2();
+
                 Intent intent = new Intent(QRActivity.this, library2Activity.class);
                 startActivity(intent);
             } else if (scannedContent.equalsIgnoreCase("Library Ground Floor Exit")) {
+                // check if room is full
+                if (libRoom1 <= 0) {
+                    txtScan.setText("Room is empty, try again later");
+                    return;
+                }
+
                 txtScan.setText("QR Code successfully scanned: " + scannedContent);
-                room1.setValue(libRoom1 - 1);
+
+                // insert to database
+                room1.child("Current").setValue(libRoom1 - 1);
+                outsertOnRoom1();
+
                 txtScan.setText("Exit scanned. See you again!");
-                //successful notif muna dapat dito
                 Intent intent = new Intent(QRActivity.this, mapActivity.class);
                 startActivity(intent);
             } else if (scannedContent.equalsIgnoreCase("Library Second Floor Exit")) {
+                // check if room is full
+                if (libRoom2 <= 0) {
+                    txtScan.setText("Room is empty, try again later");
+                    return;
+                }
+
                 txtScan.setText("QR Code successfully scanned: " + scannedContent);
-                room2.setValue(libRoom2 - 1);
+
+                // insert to database
+                room2.child("Current").setValue(libRoom2 - 1);
+                outsertOnRoom2();
+
                 txtScan.setText("Exit scanned. See you again!");
                 //successful notif muna dapat dito
                 Intent intent = new Intent(QRActivity.this, library2Activity.class);
-               startActivity(intent);
-            }else {
+                startActivity(intent);
+            } else {
                 txtScan.setText("Invalid QR Code, try again");
             }
             mCodeScanner.startPreview();
         }));
 
-        scannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
-            }
-        });
+        scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
     }
 
     @Override
@@ -140,9 +215,9 @@ public class QRActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    private void setupPermissions(){
+    private void setupPermissions() {
         int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (permission != PackageManager.PERMISSION_GRANTED){
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             makeRequest();
         }
     }
@@ -158,14 +233,112 @@ public class QRActivity extends AppCompatActivity {
             case CAMERA_REQUEST_CODE:
                 if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "You need camera permission to use the scanner", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Successful
-                }
+                }  // Successful
+
                 break;
         }
     }
 
-    public static final int CAMERA_REQUEST_CODE = 101;
+    private void outsertOnRoom1() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dateAndTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
+        room1.child(date).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("account_id", user.getId());
+                    data.put("in", false);
+                    data.put("out", true);
+                    data.put("date_and_time", dateAndTime);
+
+                    room1.child("History").child(date).child(user.getId()+"out").setValue(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void outsertOnRoom2() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dateAndTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+        room2.child(date).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("account_id", user.getId());
+                    data.put("in", false);
+                    data.put("out", true);
+                    data.put("date_and_time", dateAndTime);
+
+                    room2.child("History").child(date).child(user.getId()+"out").setValue(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void insertOnRoom1() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dateAndTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+        room1.child(date).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("account_id", user.getId());
+                    data.put("in", true);
+                    data.put("out", false);
+                    data.put("date_and_time", dateAndTime);
+
+                    room1.child("History").child(date).child(user.getId()+"in").setValue(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void insertOnRoom2() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dateAndTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+        room2.child(date).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("account_id", user.getId());
+                    data.put("in", true);
+                    data.put("out", false);
+                    data.put("date_and_time", dateAndTime);
+
+                    room2.child("History").child(date).child(user.getId()+"in").setValue(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public static final int CAMERA_REQUEST_CODE = 101;
 }
 
