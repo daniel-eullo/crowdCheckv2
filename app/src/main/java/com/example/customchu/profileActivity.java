f116daecbc1cbf252dce314f77cb690744319460
kreios777 <kreios722@gmail.com>
2024-05-20 12:25:58 +0800

    added detailed logging on update profile

--- app/src/main/java/com/example/customchu/profileActivity.java
+++ app/src/main/java/com/example/customchu/profileActivity.java
@@ -148,7 +148,6 @@ public class profileActivity extends AppCompatActivity {
             // Fetch the student number and UID from the UI
             String studentNumber = this.studentNumber.getText().toString();
             String userId = uid.getText().toString();
-            Integer uidCur = 0;
 
             // Check if the student number or UID is empty
             if (studentNumber.isEmpty() || userId.isEmpty()) {
@@ -158,6 +157,10 @@ public class profileActivity extends AppCompatActivity {
 
             // Fetch the currently signed-in account
             GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            if (account == null) {
+                Toast.makeText(this, "User is not signed in", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             // Create a reference to the profile
             DatabaseReference profileReference = DB.child("Profiles").child(account.getId());
@@ -166,30 +169,41 @@ public class profileActivity extends AppCompatActivity {
             profile = new Profile(Integer.parseInt(studentNumber));
 
             // Set the value of the profile
-            profileReference.setValue(profile);
+            profileReference.setValue(profile).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // Set the UID in the profile
+                    DatabaseReference userIdReference = profileReference.child("uid");
+                    userIdReference.setValue(Integer.parseInt(userId)).addOnCompleteListener(task1 -> {
+                        if (task1.isSuccessful()) {
+                            // Get the name of the user
+                            String userName = account.getGivenName() + " " + account.getFamilyName();
 
-            // Create or update the profile in the database
-            profileReference.setValue(profile);
-
-            // Set the UID in the profile
-            DatabaseReference userIdReference = profileReference.child("uid");
-            userIdReference.setValue(Integer.parseInt(userId));
-
-            // Get the name of the user
-            String userName = account.getGivenName() + " " + account.getFamilyName();
-
-            // Update the user's name in the users node
-            uidCur = Integer.parseInt(userId);
-            DatabaseReference nameReference = DB.child("users").child(uidCur.toString()).child("student_name");
-            nameReference.setValue(userName);
-
-            // Notify the user that the profile has been updated
-            Toast.makeText(this, "Profile updated", Toast.LENGTH_SHORT).show();
+                            // Update the user's name in the users node
+                            DatabaseReference nameReference = DB.child("users").child(userId).child("student_name");
+                            nameReference.setValue(userName).addOnCompleteListener(task2 -> {
+                                if (task2.isSuccessful()) {
+                                    Toast.makeText(this, "Profile updated", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Log.e("ProfileUpdate", "Error updating user name", task2.getException());
+                                    Toast.makeText(this, "Error updating user name", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            Log.e("ProfileUpdate", "Error updating UID", task1.getException());
+                            Toast.makeText(this, "Error updating UID", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Log.e("ProfileUpdate", "Error updating profile", task.getException());
+                    Toast.makeText(this, "Error updating profile", Toast.LENGTH_SHORT).show();
+                }
+            });
         } catch (Exception e) {
-            // Notify the user that there has been an error
+            Log.e("ProfileUpdate", "Exception during profile update", e);
             Toast.makeText(this, "There has been an error. Profile has not been updated", Toast.LENGTH_SHORT).show();
         }
     }
+
 
     // Method to copy text to clipboard
 
